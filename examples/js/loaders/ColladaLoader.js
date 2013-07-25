f3d028e9757744d22589458e6301e810d3d4e351
Steven Carr <steven.d.carr@lmco.com>
2013-07-25 12:47:25 -0400

    Add support for both transparent modes in the collada loader

--- examples/js/loaders/ColladaLoader.js
+++ examples/js/loaders/ColladaLoader.js
@@ -3397,10 +3397,21 @@ THREE.ColladaLoader = function () {
 		if (this['transparency'] !== undefined && this['transparent'] !== undefined) {
 			// convert transparent color RBG to average value
 			var transparentColor = this['transparent'];
-			var transparencyLevel = (this.transparent.color.r +
-										this.transparent.color.g + 
-										this.transparent.color.b)
-										/ 3 * this.transparency;
+			var transparencyLevel = 0;
+			
+			// Support both transparent modes
+			if(transparentColor.opaque == "A_ONE") {
+				transparencyLevel = (3 - this.transparent.color.r -
+					this.transparent.color.g - 
+					this.transparent.color.b) / 
+					3 * this.transparency;
+			}
+			else if(transparentColor.opaque == "RGB_ZERO") {
+				transparencyLevel = (this.transparent.color.r +
+					this.transparent.color.g + 
+					this.transparent.color.b) / 
+					3 * this.transparency;
+			}
 			
 			if (transparencyLevel > 0) {
 				transparent = true;
